9438187daafd96009f490539f501e0e180062da0
Łukasz Wencel <65486779+lwencel-priv@users.noreply.github.com>
2023-11-10 14:51:40 +0100

    update for remote debug

--- utils/network_policy_manager/main.py
+++ utils/network_policy_manager/main.py
@@ -1,69 +1,13 @@
+import os
+import sys
+file_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(file_dir)
+
 import subprocess
 import json
 from typing import List, Iterator
 from network_policy_manager.network_policy.rule import NetworkPolicyRule
 from network_policy_manager.network_policy import NetworkPolicy
-
-
-DATA = {
-    "flow": {
-        "time": "2023-11-05T20:19:40.823892249Z",
-        "uuid": "6040efc3-7c9b-4355-942a-712716be5eb1",
-        "verdict": "AUDIT",
-        "ethernet": {
-            "source": "ea:a8:f9:05:05:5e",
-            "destination": "26:ac:ac:fd:d3:3a"
-        },
-        "IP": {
-            "source": "10.244.1.147",
-            "destination": "172.18.0.3",
-            "ipVersion": "IPv4"
-        },
-        "l4": {
-            "TCP": {
-                "source_port": 47960,
-                "destination_port": 6443,
-                "flags": {
-                    "SYN": True
-                }
-            }
-        },
-        "source": {
-            "identity": 8757,
-            "namespace": "flux-system",
-            "labels": [
-                "k8s:app=source-controller",
-                "k8s:io.cilium.k8s.namespace.labels.app.kubernetes.io/instance=flux-system",
-                "k8s:io.cilium.k8s.namespace.labels.app.kubernetes.io/part-of=flux",
-                "k8s:io.cilium.k8s.namespace.labels.app.kubernetes.io/version=v2.1.2",
-                "k8s:io.cilium.k8s.namespace.labels.kubernetes.io/metadata.name=flux-system",
-                "k8s:io.cilium.k8s.namespace.labels.pod-security.kubernetes.io/warn-version=latest",
-                "k8s:io.cilium.k8s.namespace.labels.pod-security.kubernetes.io/warn=restricted",
-                "k8s:io.cilium.k8s.policy.cluster=default",
-                "k8s:io.cilium.k8s.policy.serviceaccount=source-controller",
-                "k8s:io.kubernetes.pod.namespace=flux-system"
-            ],
-            "pod_name": "source-controller-9dfbc5cd-cz76x"
-        },
-        "destination": {
-            "identity": 1,
-            "labels": [
-                "reserved:host",
-                "reserved:kube-apiserver"
-            ]
-        },
-        "Type": "L3_L4",
-        "node_name": "kind-control-plane",
-        "event_type": {
-            "type": 5
-        },
-        "traffic_direction": "INGRESS",
-        "is_reply": False,
-        "Summary": "TCP Flags: SYN"
-    },
-    "node_name": "kind-control-plane",
-    "time": "2023-11-05T20:19:40.823892249Z"
-}
 
 
 class NetworkPolicyManager:
