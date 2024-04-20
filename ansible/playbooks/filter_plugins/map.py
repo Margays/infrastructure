e6562bc60a1b1f1e9e0210c24e9dd50730360dcd
Lukasz Wencel <65486779+lwencel-priv@users.noreply.github.com>
2024-04-21 00:12:00 +0200

    fix pre-commit

--- ansible/playbooks/filter_plugins/map.py
+++ ansible/playbooks/filter_plugins/map.py
@@ -3,10 +3,10 @@ class FilterModule(object):
         return {
             'map_to_key_value': self.map_to_key_value,
         }
-    
+
     def map_to_key_value(self, data):
         result = ""
         for k in data:
             result += (',' if result else '') + k + '=' + data[k]
-    
+
         return result
